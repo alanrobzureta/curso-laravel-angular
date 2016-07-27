f60241e354cad4dcc55c37db4888ca11c4125972
alan.gomes <alan.gomes@infor11.poli.fiocruz.br>
2016-07-27 14:28:12 -0300

    UPLOAD DE ARQUIVOS  - Criando ProjectFile

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -38,4 +38,8 @@ class Project extends Model
     {
         return $this->belongsToMany('CodeProject\Entities\User','project_members');
     }
+    
+    public function files() {
+        return $this->hasMany(ProjectFile::class);
+    }
 }
